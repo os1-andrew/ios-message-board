e36e712803dbd1f502434055cf0ae09aeb5ef7ed
Andrew Liao <andrewoliao@gmail.com>
2018-08-08 15:46:17 -0700

    Implement storyboards

---
+++ Message Board/Message Board/View Controllers/MessageDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Andrew Dhan on 8/8/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func send(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var textField: UITextView!
+}
